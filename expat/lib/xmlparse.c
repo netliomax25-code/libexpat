b05dba4aa9977e3eb6ab0c444a5432c9c6d55335
Sebastian Pipping <sebastian@pipping.org>
2026-05-26 15:21:28 +0200

    lib: Protect `XML_ParserFree` from being called from a handler

--- expat/lib/xmlparse.c
+++ expat/lib/xmlparse.c
@@ -1842,7 +1842,7 @@ void XMLCALL
 XML_ParserFree(XML_Parser parser) {
   TAG *tagList;
   OPEN_INTERNAL_ENTITY *entityList;
-  if (parser == NULL)
+  if ((parser == NULL) || parser->m_insideHandler)
     return;
   /* free m_tagStack and m_freeTagList */
   tagList = parser->m_tagStack;
